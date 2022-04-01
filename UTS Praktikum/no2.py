9ef1510383794abcde9451bb0849c5a632e705de
yogifirmansyah14 <91444265+yogifirmansyah14@users.noreply.github.com>
2022-04-01 10:35:07 +0700

    Add files via upload

---
+++ UTS Praktikum/no2.py
@@ -0,0 +1,37 @@
+# Moch Yogi Firmansyah
+# 20051397044
+# D4 Manajamen Informatika 2020B
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-6, 6, -6, 6, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glColor3f(1, 1, 1)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(0.00, 4.00)
+    glVertex2f(4.00, 4.00)
+    glVertex2f(4.00, 0.00)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(0.00, 4.00)
+    glEnd()
+
+    glutSwapBuffers()
+
+
+glutInit(sys.argv)
+glutInitWindowSize(500, 500)
+glutCreateWindow("UTS GRAFIKA KOMPUTER")
+glutDisplayFunc(draw)
+glutMainLoop()
